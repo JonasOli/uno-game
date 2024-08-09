73e30346b52cdb85d66b04d56e4ac80b0549c410
Jonas Oliveira <jonasfissicaro13@gmail.com>
2024-08-08 21:31:46 -0300

    add test

--- src/application/test/game.test.ts
+++ src/application/test/game.test.ts
@@ -1,3 +1,4 @@
+import Game from '../../repository/models/Game';
 import GameSession from '../../repository/models/GameSession';
 import Player from '../../repository/models/Player';
 import { errors } from '../../util/statusMessages';
@@ -48,6 +49,33 @@ describe('Game', () => {
         await startGame(1, 'test@email.com');
       })
         .rejects.toThrow(errors.ONLY_GAME_OWNER_CAN_START_GAME)
+        .finally(() => {
+          done();
+        });
+    });
+
+    it('should return an error if trying to start a game that is already started', (done) => {
+      const mockResults = [
+        { id: 1, gameId: 1 },
+        { id: 2, gameId: 1 },
+        { id: 3, gameId: 1 },
+        { id: 4, gameId: 1 },
+      ];
+      // @ts-ignore
+      jest.spyOn(GameSession, 'findAll').mockResolvedValue(mockResults);
+      jest.spyOn(Player, 'findOne').mockResolvedValue({
+        // @ts-ignore
+        id: 1,
+      });
+      // @ts-ignore
+      jest.spyOn(GameSessionApp, 'getGameSession').mockResolvedValue({ id: 1, gameId: 1, isOwner: true });
+      // @ts-ignore
+      jest.spyOn(Game, 'findByPk').mockResolvedValue({ status: 'started' });
+
+      expect(async () => {
+        await startGame(1, 'test@email.com');
+      })
+        .rejects.toThrow(errors.GAME_ALREADY_STARTED)
         .finally(() => {
           done();
         });
